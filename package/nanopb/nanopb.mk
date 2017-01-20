feda08f1d832fcbdf1701f4e187c59da9b4e9fa1
William A. Kennington III <wak@google.com>
2017-01-20 13:18:24 -0800

    WIP

--- package/nanopb/nanopb.mk
+++ package/nanopb/nanopb.mk
@@ -4,11 +4,14 @@
 #
 ################################################################################
 
-NANOPB_VERSION = c946b7011a843afce09417a563e49209736de0f0
+NANOPB_VERSION = 50e989482c981631340c07ad57ec4d81dfd6a892
 NANOPB_SITE = $(call github,wak-google,nanopb,$(NANOPB_VERSION))
 NANOPB_LICENSE = BSD-3c
 NANOPB_LICENSE_FILES = LICENSE.txt
-HOST_NANOPB_DEPENDENCIES = host-protobuf host-python-protobuf
+NANOPB_INSTALL_STAGING = YES
+NANOPB_INSTALL_TARGET = NO
+NANOPB_DEPENDENCIES = host-pkgconf
+HOST_NANOPB_DEPENDENCIES = host-pkgconf host-protobuf host-python-protobuf
 NANOPB_CONF_OPTS = -Dnanopb_BUILD_RUNTIME=ON -Dnanopb_BUILD_GENERATOR=OFF
 HOST_NANOPB_CONF_OPTS = -Dnanopb_BUILD_RUNTIME=OFF -Dnanopb_BUILD_GENERATOR=ON
 
--- package/pblog/pblog.mk
+++ package/pblog/pblog.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-PBLOG_VERSION = 93b05307000ee3d6b7e4276f1cbb51d71fee4b09
-PBLOG_SITE = $(call github,google,pblog,$(PBLOG_VERSION))
+PBLOG_VERSION = ddc6ad1082d50b24602a61771341177ba7bba6e5
+PBLOG_SITE = $(call github,wak-google,pblog,$(PBLOG_VERSION))
 PBLOG_LICENSE = Apache-2.0
 PBLOG_LICENSE_FILES = LICENSE.txt
-
-PBLOG_DEPENDENCIES = host-protobuf host-nanopb nanopb
+PBLOG_INSTALL_STAGING = YES
+PBLOG_INSTALL_TARGET = NO
+PBLOG_DEPENDENCIES = host-pkgconf host-protobuf host-nanopb nanopb
 
 define PBLOG_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
@@ -19,12 +20,6 @@ endef
 define PBLOG_INSTALL_STAGING_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
 		DESTDIR=$(STAGING_DIR) \
-		-C $(@D) install
-endef
-
-define PBLOG_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
-		DESTDIR=$(TARGET_DIR) \
 		-C $(@D) install
 endef
 
